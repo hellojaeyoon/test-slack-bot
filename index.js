0529b71d48b0f5bc417e1c925eb0d07856939ded
hellojaeyoon <hellojaeyoon.lee@navercorp.com>
2023-12-15 13:02:59 +0900

    changes

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const fetch = require("node-fetch");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(aysnc () => {
+  try {
+    const payload = {
+      channel: `${core.getInput("channel")}`,
+      attachments: [
+        {
+          color: status === "success" ? "#2e993e" : status === "failure" ? "#bd0f26" : "#d29d0c",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `Github Action: *${status === "success" ? "SUCCESS" : status === "failure" ? "FAILURE" : "CANCELLED"}*`,
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    const rest = await fetch("https://slack.com/api/chat.postMessage", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Content-Length": payload.length,
+        Authorization: `Bearer ${core.getInput("slack-bot-token")}`,
+        Accept: "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`Server error ${res.status}`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
